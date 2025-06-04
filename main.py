abab808d9ed4310a32fbed42d628911d797ac6d9
drifter089 <akshat@MAIN.localdomain>
2025-06-04 20:37:57 +0700

    allow all cors

--- main.py
+++ main.py
@@ -100,10 +100,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
 )
 
 @app.get("/process/{id}")
